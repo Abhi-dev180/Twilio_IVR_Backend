ebc3a03849119c5a8b612a288ceee3b29c5dc9cf
Abhi-dev180 <rajdevfree2@gmail.com>
2026-08-20 09:02:47 +0530

    Add ws polyfill for Node 20 environments

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 dotenv.config();
 import bcrypt from 'bcryptjs';
+import WebSocket from 'ws';
 
+if (!global.WebSocket) {
+  global.WebSocket = WebSocket;
+}
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
